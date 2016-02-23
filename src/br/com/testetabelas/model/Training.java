d9ffa267b3ebdced587a5a031325fdd609970b5a
Daniel Ulrik <dulrik08@gmail.com>
2016-02-23 08:53:12 -0300

    Model/Persistence

---
+++ src/br/com/testetabelas/model/Training.java
@@ -0,0 +1,7 @@
+package br.com.testetabelas.model;
+
+/**
+ * Created by desenvolv09 on 23/02/2016.
+ */
+public class Training {
+}
---
+++ src/br/com/testetabelas/persistence/DbHandler.java
@@ -0,0 +1,29 @@
+package br.com.testetabelas.persistence;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by desenvolv09 on 23/02/2016.
+ */
+public class DbHandler extends SQLiteOpenHelper{
+
+    private static final String DB_NAME = "db";
+    private static final int DB_VERSION = 1;
+
+    public DbHandler(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table recebimento(_id INTEGER primary key autoincrement, nomeRecebimento TEXT NULL, idRecebimento INTEGER NULL);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table recebimento");
+        onCreate(db);
+    }
+}
