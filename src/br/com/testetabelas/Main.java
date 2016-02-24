f2f54673a5e24fb00c82590ab874209a9bef1af9
Daniel Ulrik <dulrik08@gmail.com>
2016-02-24 11:07:48 -0300

    Ajustes layout

--- src/br/com/testetabelas/Main.java
+++ src/br/com/testetabelas/Main.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CalendarView;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -12,6 +14,9 @@ import java.util.Calendar;
 public class Main extends Activity {
 
     CalendarView calendarView;
+    RadioButton radioButtonGroupFat, radioButtonGroupHyper;
+    RadioButton radioButtonFat1, radioButtonFat2, radioButtonFat3;
+    RadioButton radioButtonHyp1, radioButtonHyp2, radioButtonHyp3;
 
     /**
      * Called when the activity is first created.
@@ -23,20 +28,6 @@ public class Main extends Activity {
         initializeCalendar();
     }
 
-    public void initializeCalendar() {
-        calendarView = (CalendarView) findViewById(R.id.calendar);
-        calendarView.setShowWeekNumber(false);
-        calendarView.setFirstDayOfWeek(2);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(year, month, day);
-                Toast.makeText(getApplicationContext(), calendar.getTime().toString(), Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
@@ -54,6 +45,38 @@ public class Main extends Activity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeGroups() {
+        radioButtonGroupFat = (RadioButton) findViewById(R.id.group_fat);
+        radioButtonGroupHyper = (RadioButton) findViewById(R.id.group_hyper);
+
+        radioButtonGroupFat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+        radioButtonGroupHyper.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+    }
+
+    private void initializeCalendar() {
+        calendarView = (CalendarView) findViewById(R.id.calendar);
+        calendarView.setShowWeekNumber(false);
+        calendarView.setFirstDayOfWeek(2);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, day);
+                Toast.makeText(getApplicationContext(), calendar.getTime().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void createNewExercise() {
